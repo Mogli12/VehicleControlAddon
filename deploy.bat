804e8ba16947063598d191f6bda40cdda0fcca72
Stefan Biedenstein <mbiedens@gmail.com>
2016-11-04 16:02:15 +0100

    deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 @echo "mods"
-@xcopy zzzKeyboardSteer.zip "C:\Users\d027464\Documents\My Games\FarmingSimulator2017\mods" /R/U/Y/Q
+@xcopy FS17_KeyboardSteer.zip "C:\Users\d027464\Documents\My Games\FarmingSimulator2017\mods" /R/U/Y/Q
