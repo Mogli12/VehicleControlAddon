1b8d832a48e94ef971013f4a13d6466bb52cca8d
Mogli12 <mbiedens@gmail.com>
2019-04-05 09:38:24 +0200

    v0.0.4.7
    working width 1.5m..1.9m #183

--- FS19_VehicleControlAddon/vehicleControlAddon.lua
+++ FS19_VehicleControlAddon/vehicleControlAddon.lua
@@ -3013,7 +3013,7 @@ function vehicleControlAddon:vcaShowSettingsUI()
 	
 	self.vcaUI.vcaSnapDistance_V = {}
 	self.vcaUI.vcaSnapDistance   = {}
-	for i,v in pairs( { 0,
+	for i,v in pairs( { 0, 1.5, 1.6, 1.7, 1.8, 1.9,
 											2, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 
 											3, 3.1, 3.2, 3.4, 3.6, 3.8, 4, 4.2, 4.5, 4.8, 
 											5, 5.5, 6, 7, 7.5, 8, 9, 10, 12, 13.5, 15, 
