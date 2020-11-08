9fe96e8d8c2752ae0b1d4df2a824bb4ee4575655
knarzfurz <maik.thomas.it@googlemail.com>
2020-11-08 16:53:22 +0100

    Not read correctly from Transmission.xml
    GearTexts and AutoRanges not read correctly from Transmission.xml

--- FS19_VehicleControlAddon/vehicleControlAddonTransmissions.lua
+++ FS19_VehicleControlAddon/vehicleControlAddonTransmissions.lua
@@ -1104,7 +1104,7 @@ function vehicleControlAddonTransmissionBase.loadSettings()
 				
 				j = 0
 				while true do 
-					local key2 = key..string.format( ".gearTexts. gearText(%d)", j )
+					local key2 = key..string.format( ".gearTexts.gearText(%d)", j )
 					local text = getXMLString( xmlFile, key2.."#value" ) 
 				--print(tostring(key2)..": "..tostring(text))
 					if text == nil then
@@ -1148,7 +1148,7 @@ function vehicleControlAddonTransmissionBase.loadSettings()
                                    timeRanges         = timeRanges,
                                    gearRatios         = gearRatios,
                                    autoGears          = autoGears,
-                                   autoRanges´        = autoRanges,
+                                   autoRanges         = autoRanges,
                                    splitGears4Shifter = splitGears4Shifter,
                                    gearTexts          = gearTexts,
                                    rangeTexts         = rangeTexts,
